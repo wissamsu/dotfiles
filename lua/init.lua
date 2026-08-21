5f9fa10c60e3a68755d5787acc1cc881311349f1
wiss <nexuswave3@gmail.com>
2026-08-21 02:49:52 -0400

    added some lsp's support like terraform and kubernetes

--- lua/init.lua
+++ lua/init.lua
@@ -6,15 +6,18 @@ vim.g.maplocalleader = "\\"
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 local nvim_lazypath = os.getenv("HOME") .. "/.local/share/nvim/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) and vim.loop.fs_stat(nvim_lazypath) then
-  lazypath = nvim_lazypath
+	lazypath = nvim_lazypath
 end
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
-
 
 vim.opt.rtp:prepend(lazypath)
 
@@ -22,3 +25,11 @@ require("lazy").setup("plugins")
 
 require("options")
 require("mappings")
+vim.opt.undofile = true
+
+local undodir = os.getenv("HOME") .. "/.local/state/nvim2/undo"
+vim.opt.undodir = undodir
+
+if vim.fn.isdirectory(undodir) == 0 then
+	vim.fn.mkdir(undodir, "p")
+end
--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -55,5 +55,70 @@ return {
       "coc-webview",
       "coc-sourcekit",
     }
+
+    local tf_init_running = {}
+    local function terraform_init(dir, reason)
+      if not dir or dir == "" or tf_init_running[dir] then return end
+      tf_init_running[dir] = true
+      vim.notify("terraform: running init (" .. (reason or "for LSP completions") .. ")...")
+      vim.system({ "terraform", "init", "-no-color", "-input=false" }, { cwd = dir, text = true }, function(res)
+        vim.schedule(function()
+          tf_init_running[dir] = nil
+          if res.code == 0 then
+            vim.notify("terraform init done - provider schemas will load shortly")
+          else
+            vim.notify("terraform init failed:\n" .. (res.stderr or res.stdout or ""), vim.log.levels.WARN)
+          end
+        end)
+      end)
+    end
+
+    local function buf_providers(text)
+      local set = {}
+      for p in text:gmatch('provider%s+"([%w-]+)"') do set[p] = true end
+      for blk in text:gmatch("required_providers%s*{(.-)}") do
+        for name in blk:gmatch("([%w-]+)%s*=") do set[name] = true end
+      end
+      return set
+    end
+
+    local function missing_providers(dir, text)
+      local lock = vim.fs.joinpath(dir, ".terraform.lock.hcl")
+      local f = io.open(lock, "r")
+      if not f then return true end
+      local locked = f:read("*a") or ""
+      f:close()
+      for p in pairs(buf_providers(text)) do
+        if not locked:find("/" .. p .. '"', 1, true) then return true end
+      end
+      return false
+    end
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "terraform",
+      callback = function(args)
+        local dir = vim.fs.dirname(args.file)
+        if not dir or dir == "" then return end
+        if not vim.uv.fs_stat(vim.fs.joinpath(dir, ".terraform")) then
+          terraform_init(dir, "first open in new project")
+        end
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      pattern = { "*.tf", "*.tfvars" },
+      callback = function(args)
+        local dir = vim.fs.dirname(args.file)
+        if not dir or dir == "" then return end
+        local text = table.concat(vim.api.nvim_buf_get_lines(args.buf, 0, -1, false), "\n")
+        if next(buf_providers(text)) and missing_providers(dir, text) then
+          terraform_init(dir, "new providers detected")
+        end
+      end,
+    })
+
+    vim.api.nvim_create_user_command("TerraformInit", function()
+      terraform_init(vim.fs.dirname(vim.api.nvim_buf_get_name(0)), "manual request")
+    end, { desc = "Run terraform init in buffer's directory for LSP schemas" })
   end,
 }
