a535d0d285dc8249f1e4b30eaa626bcdf941b47a
wiss <nexuswave3@gmail.com>
2026-08-23 23:33:55 -0400

    save

--- lua/init.lua
+++ lua/init.lua
@@ -6,17 +6,17 @@ vim.g.maplocalleader = "\\"
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 local nvim_lazypath = os.getenv("HOME") .. "/.local/share/nvim/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) and vim.loop.fs_stat(nvim_lazypath) then
-	lazypath = nvim_lazypath
+  lazypath = nvim_lazypath
 end
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -31,5 +31,5 @@ local undodir = os.getenv("HOME") .. "/.local/state/nvim2/undo"
 vim.opt.undodir = undodir
 
 if vim.fn.isdirectory(undodir) == 0 then
-	vim.fn.mkdir(undodir, "p")
+  vim.fn.mkdir(undodir, "p")
 end
