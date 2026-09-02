3f50a41aa6e2f420b80c6607103575a2689c0b7e
wiss <nexuswave3@gmail.com>
2026-09-02 00:40:36 -0400

    save

--- lua/plugins/vimspector.lua
+++ lua/plugins/vimspector.lua
@@ -1,6 +1,6 @@
 return {
   "puremourning/vimspector",
-  ft = { "python", "go", "rust", "java" },
+  ft = { "java" },
   cmd = { "VimspectorInstall", "VimspectorUpdate" },
   fn = { "vimspector#Launch()", "vimspector#ToggleBreakpoint()" },
   init = function()
@@ -18,5 +18,14 @@ return {
   end,
   config = function()
     vim.g.vimspector_enable_mappings = 'HUMAN'
+    vim.api.nvim_set_hl(0, 'VimspectorBreakpointGreen', { fg = '#9ece6a', bold = true })
+    vim.api.nvim_set_hl(0, 'VimspectorBreakpointRed', { fg = '#f7768e', bold = true })
+    vim.api.nvim_set_hl(0, 'VimspectorPCBreakpointRed', { fg = '#f7768e', bg = '#3b4252', bold = true })
+
+    -- 2. Override Vimspector signs using a bug icon (e.g., "󰃰" from Nerd Fonts, or "")
+    -- Note: Make sure you are using a Nerd Font for the bug symbol to render correctly.
+    vim.fn.sign_define('vimspectorBP', { text = '', texthl = 'VimspectorBreakpointGreen' })
+    vim.fn.sign_define('vimspectorBPDisabled', { text = '', texthl = 'Comment' })
+    vim.fn.sign_define('vimspectorPCBP', { text = '', texthl = 'VimspectorPCBreakpointRed', linehl = 'CursorLine' })
   end,
 }
